e6743c0230d95a951f815e595a2471948bd529a0
Davy'd <pdaswift3009@gmail.com>
2018-02-14 12:26:26 +0100

    user post route

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
-var restify = require('restify');
-var server = restify.createServer();
+const restify = require('restify'),
+      server = restify.createServer();
+
+var Users = {};
+var max_user_id = 0;
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+// server.use(restify.acceptParser(server.acceptable));
+server.use(restify.plugins.bodyParser());
+
+server.get('/', (req, res, next) => {
+  res.setHeader('content-type', 'application/json');
+  res.writeHead(200);
+  res.end(JSON.stringify(Users));
+  return next();
+});
+
+
+
+server.post('/user', (req, res, next) => {
+  var user = req.params;
+  max_user_id += 1;
+  user.id = max_user_id;
+  Users[user.id] = user;
+
+  res.setHeader('content-type', 'application/json');
+  res.writeHead(200);
+  res.end(JSON.stringify(user));
+  return next();
+})
+
+
 
 
 
