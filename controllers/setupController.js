fc99cd692829d5073e526c39e2e6b07d73de882e
Davy'd <pdaswift3009@gmail.com>
2018-02-14 19:02:51 +0100

    use api key with basic auth

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -1,3 +1,5 @@
+var helpers = require('../config/helpers');
+
 module.exports = function(server, restify, restifyValidator) {
 
   server.use(restify.plugins.acceptParser(server.acceptable));
@@ -5,4 +7,28 @@ module.exports = function(server, restify, restifyValidator) {
   server.use(restify.plugins.bodyParser());
   server.use(restifyValidator);
  
+  server.use(restify.plugins.authorizationParser());
+
+  server.use((req, res, next) => {
+      
+      var apiKeys = {
+     //username:  password
+        'user1': 'jdueoabjieonwfduc8374msgw93'
+      };
+
+
+      if(typeof(req.authorization.basic) ==='undefined' || !apiKeys[req.authorization.basic.usename] || req.authorization.password !== apiKeys[req.authorization.basic.username]){
+         return helpers.failure(res, next, "Must specify a valid API key", 403)
+        // var response = {
+        //   'status': 'failure',
+        //   'data':  "Must specify a valid API key"
+        // };  
+        // res.setHeader('content-type', 'application/json');
+        // res.writeHead(403);
+        // res.end(JSON.stringify(response));
+        // return next();
+      }
+
+      return next();
+  });
 }
